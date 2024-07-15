80a849697c16c96ec364cc0af9044c4a74d9242e
daisuke721 <d.magome.07@gmail.com>
2024-07-16 03:49:09 +0900

    step2: パスワードを保存&出力する処理を作成

--- password_manager.sh
+++ password_manager.sh
@@ -1,16 +1,48 @@
 #!/bin/bash
 
-echo "パスワードマネージャーへようこそ！"
+echo "パスワードマネージャーへようこそ!"
+echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 
-echo "サービス名を入力してください:"
-read sarvice_name
+while true; do
+	read options
 
-echo "ユーザー名を入力してください:"
-read user_name
+	case $options in
+		"Add Password")
+			echo "サービス名を入力してください:"
+			read sarvice_name
 
-echo "パスワードを入力してください:"
-read -s password
+			echo "ユーザー名を入力してください:"
+			read user_name
 
-echo "サービス名:$sarvice_name/ユーザー名:$user_name/パスワード:$password" >> pass_list.txt
+			echo "パスワードを入力してください"
+			read -s password
 
-echo "Thank you!"
+			echo "サービス名:$sarvice_name/ユーザー名:$user_name/パスワード:$password" >> pass_list.txt
+
+			echo "パスワードの追加は成功しました。"
+			echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+			;;
+		"Get Password")
+			echo "サービス名を入力してください:"
+			read sarvice_name
+
+			password=$(grep "$sarvice_name" pass_list.txt | cut -d / -f 3)
+
+			if [ -z $password ]; then
+				echo "そのサービス名は登録されていません。"
+			else
+				echo "$(grep $sarvice_name pass_list.txt | cut -d / -f 1)"
+				echo "$(grep $sarvice_name pass_list.txt | cut -d / -f 2)"
+				echo "$password"
+			fi 
+			echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
+			;;
+		"Exit")
+			echo "Thank you!"
+			exit
+			;;
+		*)
+			echo "入力が間違えています。Add Password/Get Password/Exit から入力してください。"
+			;;
+	esac
+done
