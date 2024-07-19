8ea78edce606cda4ff1e4c7bf1fc97bae1781754
daisuke721 <d.magome.07@gmail.com>
2024-07-20 00:32:23 +0900

    step3: パスワードが保存されたファイルの暗号化

--- password_manager.sh
+++ password_manager.sh
@@ -8,8 +8,11 @@ while true; do
 
 	case $options in
 		"Add Password")
+			echo "GPGで設定したパスワードを入力してください。"
+			read gpg_email
+
 			echo "サービス名を入力してください:"
-			read sarvice_name
+			read service_name
 
 			echo "ユーザー名を入力してください:"
 			read user_name
@@ -17,24 +20,35 @@ while true; do
 			echo "パスワードを入力してください"
 			read -s password
 
-			echo "サービス名:$sarvice_name/ユーザー名:$user_name/パスワード:$password" >> pass_list.txt
+			echo "サービス名:$service_name/ユーザー名:$user_name/パスワード:$password" >> pass_list.txt
 
-			echo "パスワードの追加は成功しました。"
+			gpg --encrypt --recipient "$gpg_email" pass_list.txt 2> /dev/null
+			encryption=$?
+			
+			if [ $encryption -eq 0 ]; then
+				echo "パスワードの追加は成功しました。"
+			else
+				echo "パスワードの追加に失敗しました。もう一度、やり直してください。"
+			fi
+			rm pass_list.txt
 			echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 			;;
 		"Get Password")
 			echo "サービス名を入力してください:"
-			read sarvice_name
+			read service_name
 
-			password=$(grep "$sarvice_name" pass_list.txt | cut -d / -f 3)
+			gpg pass_list.txt.gpg 2> /dev/null
+
+			password=$(grep "$service_name" pass_list.txt | awk -F '[:/]' -v 'OFS=:' '{print $5,$6}')
 
 			if [ -z $password ]; then
 				echo "そのサービス名は登録されていません。"
 			else
-				echo "$(grep $sarvice_name pass_list.txt | cut -d / -f 1)"
-				echo "$(grep $sarvice_name pass_list.txt | cut -d / -f 2)"
+				echo "$(grep $service_name pass_list.txt | awk -F '[:/]' -v 'OFS=:' '{print $1,$2}')"
+				echo "$(grep $service_name pass_list.txt | awk -F '[:/]' -v 'OFS=:' '{print $3,$4}')"
 				echo "$password"
-			fi 
+			fi
+			rm pass_list.txt
 			echo "次の選択肢から入力してください(Add Password/Get Password/Exit):"
 			;;
 		"Exit")
